e0f950ff326387ccb781d0d024756699a46bfa04
naveen-2111-dev <naveenrajanm9@gmail.com>
2025-11-16 17:39:02 +0530

    new vercel

--- index.ts
+++ index.ts
@@ -1,8 +1,7 @@
 import { configDotenv } from "dotenv";
-import app from "./src/app"; configDotenv();
+
+configDotenv();
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+export { default } from "./src/app";
