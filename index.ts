5152364770242fde431b6eacce2df825246819d4
naveen-2111-dev <naveenrajanm9@gmail.com>
2025-11-16 17:54:55 +0530

    main!!!

--- index.ts
+++ index.ts
@@ -1,5 +1,12 @@
 import { configDotenv } from "dotenv";
+import app from "./src/app";
 
 configDotenv();
 
-export { default } from "./src/app";
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+export default app;
